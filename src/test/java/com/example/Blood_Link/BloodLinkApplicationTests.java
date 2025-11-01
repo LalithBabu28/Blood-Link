d0c8b1574ba813bdca6e76e001b81e3dec1b7a19
SANJAY <kavin>
2025-11-01 15:13:24 +0530

    Initial commit

---
+++ src/test/java/com/example/Blood_Link/BloodLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blood_Link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
